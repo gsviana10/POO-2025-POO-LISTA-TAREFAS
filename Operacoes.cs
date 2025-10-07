dd018929bc5e5660ed91ed2c20d661166c643e59
gsviana10 <georgesamuel.sa@gmail.com>
2025-10-07 00:17:56 +0000

    aula 06

---
+++ Operacoes.cs
@@ -0,0 +1,26 @@
+public class Operacoes
+{
+    public int Criar(tarefa tarefa)
+{
+return 0:
+}
+
+public Tarefa Buscar(int id)
+{
+    return null:
+}
+
+public List<Tarefa> Listar()
+{
+return Array. empty<Tarefa>();
+}
+
+public void Alterar (Tarefa tarefa)
+{
+
+}
+
+public void excluir (int id)
+{
+    
+}
---
+++ tarefa/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Tarefa
+{
+
+    public int id {get; set;}
+    public string Nome {get; set;}
+    public string Descricao {get; set;}
+    public DateTime DataCriacao {get; set;}
+    public int Status {get; set;}
+    public DateTime DataExecucao {get; set;}
+    
+    
+}
